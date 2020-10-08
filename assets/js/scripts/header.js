097dc485c873581a6998b8becc8340f7e9e7566c
Valery Juravlev <valera030219972010@gmail.com>
2020-10-08 21:26:38 +0300

    popup 4

--- assets/js/scripts/header.js
+++ assets/js/scripts/header.js
@@ -6,6 +6,9 @@ const menuClose = document.querySelector('#menu_close');
 const modalHomePage = document.querySelector('#modal_home-page');
 const modalCloseHomePage = document.querySelector('#popup_close_home-page');
 const openPopupHomePage = document.querySelector('#popup_open_home-page');
+const openPopupBasketPage = document.querySelector('#popup_open_basket-page');
+const popupBasketPage = document.querySelector('#modal_basket-page');
+const closePopupBasketPage = document.querySelector('#popup_close_basket-page');
 
 function openModal(el, className) {
     if(!el.classList.contains(className)) {
@@ -33,7 +36,15 @@ openPopupHomePage.addEventListener('click', function() {
 
 modalCloseHomePage.addEventListener('click', function() {
     closeModal(modalHomePage, 'popup_open');
-})
+});
+
+openPopupBasketPage.addEventListener('click', function() {
+    openModal(popupBasketPage, 'popup_open');
+});
+
+closePopupBasketPage.addEventListener('click', function() {
+    closeModal(popupBasketPage, 'popup_open');
+});
 
 /*menu close*/
 
